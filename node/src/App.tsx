45edf8803a7274235f8776b28f681021ad632cad
Jack <62914920+Jack-a-Lope@users.noreply.github.com>
2024-12-03 20:56:08 -0500

    Update App.tsx
    I added comments to the Radix Sort code :)

--- node/src/App.tsx
+++ node/src/App.tsx
@@ -114,30 +114,30 @@ function App() {
     setT1(end - start);
 
     // radix sort
-    start = performance.now();
-    if (alg_2_list.length > 0) {
+        start = performance.now();
+    if (alg_2_list.length > 0) { //This code checks the array of pokemon to find the largest use rate and in turn the most digits that a use rate can have
       var temp_max = alg_2_list[0];
       alg_2_list.forEach((p) => {
         if (p.usage > temp_max.usage) {
-          temp_max = p;
+          temp_max = p; 
         }
       });
 
-      var max = temp_max.usage.toString().length;
+      var max = temp_max.usage.toString().length; //max is the overall maximum number of digits a usage rate can have
       var i = 0;
       var div = 10;
 
-      while (i < max) {
-        let list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      while (i < max) { //This is a loop that runs d times, once for digit in the max usage
+        let list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; //An array correlating to digits 0 through 10 is created to store the frequency that different values at a certain place are those numbers
         alg_2_list.forEach((p) => {
           var num = Math.floor(p.usage / (10));
           list[(num % 10)]++;
         });
-        for (let j = 1; j < list.length; j++) {
-          list[j] = list[j] + list[(j - 1)];
+        for (let j = 1; j < list.length; j++) { //This code turns the individual frequency rates into a cumulative rate now referencing the last index of the original pokedex array where a value with the index
+          list[j] = list[j] + list[(j - 1)];    //of list has at a given digit
         }
         let temp_mons = [];
-        for (let p = alg_2_list.length - 1; p >= 0; p--) {
+        for (let p = alg_2_list.length - 1; p >= 0; p--) { //This code takes the new list and places values in their appropriate places back-to front, sorting them by the givin numerical place
           var num = ((Math.floor(alg_2_list[p].usage / (10))) % 10);
           list[num] = list[num] - 1;
           temp_mons[list[num]] = alg_2_list[p];
@@ -152,7 +152,7 @@ function App() {
         //alg_1_list[0].name = list[0].toString();
       }
 
-      alg_2_list.reverse();
+      alg_2_list.reverse(); //Radix sort results in ascending order, but for this project we want to display results in descending order so the final result is reversed
     }
     end = performance.now();
     setT2(end - start);
